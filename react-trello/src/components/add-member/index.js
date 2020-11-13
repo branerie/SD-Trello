627dc72b8c473a09746d5b3c230664da467d68e4
BozhidarDimitrow <71080041+BozhidarDimitrow@users.noreply.github.com>
2020-11-13 09:45:21 +0200

    get all users rest api

--- react-trello/src/components/add-member/index.js
+++ react-trello/src/components/add-member/index.js
@@ -26,7 +26,7 @@ export default function AddMember(props) {
 
     const deleteMember = useCallback(async (event, member) => {
         event.preventDefault()
-        
+
         var index = members.indexOf(member)
         if (index !== -1) {
             members.splice(index, 1)
@@ -52,9 +52,23 @@ export default function AddMember(props) {
     }, [history, props, cardId, listId])
 
 
-    const handleAdd = useCallback(async (event) => {
+    const handleAdd = useCallback(async (event, email) => {
         event.preventDefault()
+
         const token = getCookie("x-auth-token")
+        const user = await fetch(`http://localhost:4000/api/user/${email}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+            }
+        })
+        if (!user.ok) {
+            history.push("/error")
+        }
+        console.log(user);
+        members.push(user)
+        console.log(members);
         const response = await fetch(`http://localhost:4000/api/projects/lists/cards/${listId}/${cardId}`, {
             method: "PUT",
             headers: {
@@ -62,7 +76,7 @@ export default function AddMember(props) {
                 "Authorization": token
             },
             body: JSON.stringify({
-                members: []
+                members
             })
         })
         if (!response.ok) {
@@ -71,26 +85,31 @@ export default function AddMember(props) {
             props.hideFormAdd()
         }
 
-    }, [history, members, listId, cardId, props])
+    }, [history, listId, cardId, props])
 
     return (
         <div className={styles.form}>
-            <form className={styles.container} >
-                <Title title={"Select member"} />
+            <div className={styles.currentMembers}>
+                <Title title={"Card members"} />
                 {
                     members.map((element, index) => {
-                        return <Button key={index} onClick={(e) => { if (window.confirm('Are you sure you wish to delete this member?')) deleteMember(e._id) }} title={element.username} />
+                        return <Button key={index} onClick={(e) => { if (window.confirm('Are you sure you wish to delete this member?')) deleteMember(e, element) }} title={element.username} />
                     })
                 }
-                <Input
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    label="Enter User email"
-                    id="email"
-                />
-                <Button onClick={handleAdd} title="+" />
-                <Button onClick={cancelAdd} title="Cancel" />
-            </form>
+            </div>
+            <div>
+                <form className={styles.container} >
+                    <Title title={"Add new member"} />
+                    <Input
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        label="Enter User email"
+                        id="email"
+                    />
+                    <Button onClick={handleAdd} title="Add" />
+                    <Button onClick={cancelAdd} title="Cancel" />
+                </form>
+            </div>
         </div>
     )
 }
