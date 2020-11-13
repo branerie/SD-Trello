45f08a43af1589df2318af51cc689ce0fc666128
KaloyanDimitrov-sd <71079141+KaloyanDimitrov-sd@users.noreply.github.com>
2020-11-13 12:22:31 +0200

    Edit, delete list and project added in React

---
+++ react-trello/src/components/edit-project/index.js
@@ -0,0 +1,67 @@
+import React, { useCallback, useState } from 'react'
+import { useHistory } from "react-router-dom"
+import Button from '../button'
+import Input from '../input'
+import Title from '../title'
+import styles from './index.module.css'
+import getCookie from '../../utils/cookie'
+import "react-datepicker/dist/react-datepicker.css"
+import { useSocket } from '../../contexts/SocketProvider'
+
+
+export default function EditProject(props) {
+    const [name, setName] = useState(props.project.name)
+    const [description, setDescription] = useState(props.project.description)
+    const history = useHistory()
+    const socket = useSocket()
+
+    const projectId = props.project._id
+
+    const updateProjectSocket = useCallback(() => {
+        socket.emit('project-update', props.project)
+    }, [socket, props.project])
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        const token = getCookie("x-auth-token")
+        const response = await fetch(`http://localhost:4000/api/projects/${projectId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+            },
+            body: JSON.stringify({
+                name,
+                description
+            })
+        })
+        if (!response.ok) {
+            history.push("/error")
+        } else {
+            updateProjectSocket()
+            props.hideForm()
+        }
+    }
+
+    return (
+        <div className={styles.form}>
+            <form className={styles.container} >
+                <Title title="Edit Project" />
+                <Input
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    label="Name"
+                    id="name"
+                />
+                <Input
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                    label="Description"
+                    id="description"
+                />
+                <Button onClick={handleSubmit} title="Edit" />
+            </form>
+        </div>
+
+    )
+}
--- react-trello/src/components/list/index.js
+++ react-trello/src/components/list/index.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useSocket } from '../../contexts/SocketProvider'
+import getCookie from '../../utils/cookie'
 import Button from '../button'
 import Card from '../card'
 import CreateCard from '../create-card'
+import EditList from '../edit-list'
 import Transparent from '../transparent'
 import styles from './index.module.css'
 
 export default function List(props) {
     const [isVisible, setIsVisible] = useState(false)
+    const [IsVisibleEdit, setIsVisibleEdit] = useState(false)
+    const history = useHistory()
+    const socket = useSocket()
 
     const showForm = () => {
         setIsVisible(true)
@@ -16,6 +23,36 @@ export default function List(props) {
         setIsVisible(false)
     }
 
+    const showFormEdit = () => {
+        setIsVisibleEdit(true)
+    }
+
+    const hideFormEdit = () => {
+        setIsVisibleEdit(false)
+    }
+
+    const updateProjectSocket = useCallback(() => {
+        socket.emit('project-update', props.project)
+    }, [socket, props.project])
+
+    async function deleteList() {
+        const token = getCookie("x-auth-token")
+        const response = await fetch(`http://localhost:4000/api/projects/lists/${props.project._id}/${props.list._id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+            }
+        })
+        if (!response.ok) {
+            history.push("/error")
+        } else {
+            updateProjectSocket()
+
+        }
+
+    }
+
     return (
         <div className={styles.list} key={props.list._id}>
             <div>
@@ -23,7 +60,7 @@ export default function List(props) {
             </div>
             {
                 props.list.cards.map((element, index) => {
-                    return <Card key={index} card={element} listId={props.list._id} project={props.project}/>
+                    return <Card key={index} card={element} listId={props.list._id} project={props.project} />
                 })
             }
             <Button title='Add card' onClick={showForm} />
@@ -31,10 +68,20 @@ export default function List(props) {
                 isVisible ?
                     <div>
                         <Transparent hideForm={hideForm}>
-                            <CreateCard hideForm={hideForm} listId={props.list._id} project={props.project}/>
+                            <CreateCard hideForm={hideForm} listId={props.list._id} project={props.project} />
                         </Transparent>
                     </div> : null
             }
+            <Button title='Edit List' onClick={showFormEdit} />
+            {
+                IsVisibleEdit ?
+                    < div >
+                        <Transparent hideForm={hideFormEdit} >
+                            <EditList hideForm={hideFormEdit} list={props.list} project={props.project} />
+                        </Transparent >
+                    </div > : null
+            }
+            <Button title='Delete List' onClick={() => { if (window.confirm('Are you sure you wish to delete this item?')) deleteList() }} />
         </div>
     )
 }
