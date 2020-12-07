42ec6b72982e507f43b5691016befc12f9bdbc55
KaloyanDimitrov-sd <71079141+KaloyanDimitrov-sd@users.noreply.github.com>
2020-12-07 10:30:35 +0200

    Log in bug fixed

--- react-trello/src/contexts/SocketProvider.js
+++ react-trello/src/contexts/SocketProvider.js
@@ -9,12 +9,15 @@ export function useSocket() {
 
 export function SocketProvider({ user, children }) {
   const [socket, setSocket] = useState()
-  const username = user.username
-  const teams = [...user.teams]
-  const teamsId = teams.map( t => t._id)
-  const teamsStr = JSON.stringify(teamsId)
-
+  
   useEffect(() => {
+    if (!user.loggedIn) {
+      return
+    }
+    const username = user.username
+    const teams = [...user.teams]
+    const teamsId = teams.map( t => t._id)
+    const teamsStr = JSON.stringify(teamsId)
     const newSocket = io(
       'http://localhost:4000', {
         query: { teamsStr, username },
