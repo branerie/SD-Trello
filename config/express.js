21350d3447ab21340568fc0606a62224b1c9d35a
KaloyanDimitrov-sd <71079141+KaloyanDimitrov-sd@users.noreply.github.com>
2020-12-08 14:29:46 +0200

    send index

--- config/express.js
+++ config/express.js
@@ -23,7 +23,7 @@ module.exports = (app) => {
   app.use(express.static(path.join(__dirname, '../build')))
 
   app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../build'))
+      res.sendFile(path.join(__dirname, '../build', 'index.html'))
   })
 
   app.use(function (err, req, res, next) {
