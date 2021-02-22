181482c67866daa53f51b7afe1fabcb14f5a893c
BozhidarDimitrow <71080041+BozhidarDimitrow@users.noreply.github.com>
2021-02-22 17:33:29 +0200

    small updates

--- src/pages/error/index.js
+++ src/pages/error/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import PageLayout from "../../components/page-layout";
+import styles from './index.module.css'
+
 
 const ErrorPage = () => {
     return (
         <PageLayout>
-            <h3>Error Page</h3>
+            <div className={styles.title}>Error Page</div>
+
+            <div className={styles.text}>
+                Something went wrong.
+            </div>
         </PageLayout>
     )
 }
