e0b448306a69bd7e0d6451cd07f465340856439b
BozhidarDimitrow <71080041+BozhidarDimitrow@users.noreply.github.com>
2021-03-08 15:52:19 +0200

    confirmation

--- src/pages/Confirmation/index.js
+++ src/pages/Confirmation/index.js
@@ -10,10 +10,10 @@ import logo from '../../images/logo.svg'
 const ConfirmationPage = () => {
     const params = useParams()
     const history = useHistory()
-    const [loading, setLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true)
     const [success, setSuccess] = useState(false)
     const userContext = useContext(UserContext)
-    const firstRegistration = userContext.user.newPasswordConfirmed
+    const isFirstRegistration = userContext.user.newPasswordConfirmed
 
 
 
@@ -31,7 +31,7 @@ const ConfirmationPage = () => {
             token
         }, (user) => {
             userContext.logIn(user)
-            setLoading(false)
+            setIsLoading(false)
             setSuccess(true)
         }, (response) => {
             console.log("Error", response)
@@ -48,25 +48,35 @@ const ConfirmationPage = () => {
         return (
             <div>
                 {
-                    firstRegistration ?
+                    isFirstRegistration ?
                         <div className={styles.container}>
                             <div className={styles.logo}>
                                 <img src={logo} alt="logo" width='194' height='142' />
                             </div>
-                            <div className={styles.title}> Confirm your email address</div >
-                            <div className={styles.text}>Please check your email to confirm your account</div>
-                            <div className={styles.text}>Once confirmed, this email address will be uniquely associated with your Smart Manager  account.</div>
-                            
+                            <div className={styles.title}>
+                                Confirm your email address
+                            </div >
+                            <div className={styles.text}>
+                                Please check your email to confirm your account
+                                </div>
+                            <div className={styles.text}>
+                                Once confirmed, this email address will be uniquely associated with your Smart Manager  account.
+                                </div>
                         </div>
                         :
                         <div className={styles.container}>
                             <div className={styles.logo}>
                                 <img src={logo} alt="logo" width='194' height='142' />
                             </div>
-                            <div className={styles.title}> Confirm your new password</div >
-                            <div className={styles.text}>Please check your email to confirm your new password</div>
-                            <div className={styles.text}>Once confirmed, your new password will be active for your Smart Manager  account.</div>
-                            
+                            <div className={styles.title}>
+                                Confirm your new password
+                                </div >
+                            <div className={styles.text}>
+                                Please check your email to confirm your new password
+                                </div>
+                            <div className={styles.text}>
+                                Once confirmed, your new password will be active for your Smart Manager  account.
+                                </div>
                         </div>
                 }
             </div>
@@ -75,22 +85,36 @@ const ConfirmationPage = () => {
 
     return (
         <div>
-            {loading && <div>Validating your email.</div>}
-            {!loading && success && <div>
-                
-                        <div className={styles.container}>
-                            <div className={styles.logo}>
-                                <img src={logo} alt="logo" width='194' height='142' />
-                            </div>
-                            <div className={styles.title}> Thank you</div >
-                            <div className={styles.text}>You can continue to Smart Manager now</div>
-                            <Button title='Proceed' className={styles['proceed-button']} onClick={() => history.push('/')} />
-                            <div className={styles.cheers}> Cheers,</div >
-                            <div className={styles.team}>The Smart Manager Team</div>
+            {
+                isLoading &&
+                <div>
+                    Validating your email.
+                </div>
+            }
+            {
+                !isLoading && success &&
+                <div>
+                    <div className={styles.container}>
+                        <div className={styles.logo}>
+                            <img src={logo} alt="logo" width='194' height='142' />
                         </div>
-                       
-
-            </div>}
+                        <div className={styles.title}>
+                            Thank you
+                        </div >
+                        <div className={styles.text}>
+                            You can continue to Smart Manager now
+                        </div>
+                        <Button title='Proceed' className={styles['proceed-button']}
+                            onClick={() => history.push('/')} />
+                        <div className={styles.cheers}>
+                            Cheers,
+                         </div >
+                        <div className={styles.team}>
+                            The Smart Manager Team
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,22 +1,20 @@
-import React from "react";
-import PageLayout from "../../components/PageLayout";
+import React from 'react'
 import styles from './index.module.css'
+import PageLayout from '../../components/PageLayout'
 import pic1 from '../../images/error-page.svg'
 
 
 
-const ErrorPage = () => {
+export default function ErrorPage() {
     return (
         <PageLayout>
             <div className={styles.title}>Error Page</div>
-        <div>
-          <img className={styles.pic1} src={pic1} alt="" />
-        </div>
-            <div className={styles.text}>
-                Something went wrong.
-            </div>
+
+            <img className={styles.pic1} src={pic1} alt="" />
+
+            <div className={styles.text}>Something went wrong.</div>
         </PageLayout>
     )
 }
 
-export default ErrorPage;
+
