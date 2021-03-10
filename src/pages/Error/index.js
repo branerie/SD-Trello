137670dfb347eab29a3eb17826774a5bb10ca9c7
b0yank <bnkaloyanov@gmail.com>
2021-03-10 11:03:24 +0200

    Added review comments for Error and Confirmation pages

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import styles from './index.module.css'
 import PageLayout from '../../components/PageLayout'
-import pic1 from '../../images/error-page.svg'
-
+import pic1 from '../../images/error-page.svg' // REVIEW: не е мн описателно pic1. errorPic или нещо такова?
+/* REVIEW:  Като цяло няма смисъл да се оставя повече от един свободен ред някъде (а тук има 3) */
 
 
 export default function ErrorPage() {
     return (
         <PageLayout>
             <div className={styles.title}>Error Page</div>
-
-            <img className={styles.pic1} src={pic1} alt='' />
+            {/* REVIEW: и за css не е много описателно styles.pic1 */}
+            <img className={styles.pic1} src={pic1} alt='' /> 
 
             <div className={styles.text}>Something went wrong.</div>
         </PageLayout>
