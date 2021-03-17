53a6241e18046af2ff16580bd769a7b034227b73
toni.todorov <toni.todorov@smartdesign.local>
2021-03-17 17:26:31 +0200

    MyTasksTask was modified cosmetically

--- src/components/MyTasksTask/index.js
+++ src/components/MyTasksTask/index.js
@@ -1,17 +1,17 @@
 import React, { useState, useRef, useEffect } from 'react'
-import ButtonClean from '../ButtonClean'
-import pen from '../../images/pen.svg'
+import { useSocket } from '../../contexts/SocketProvider'
 import styles from './index.module.css'
+import ButtonClean from '../ButtonClean'
 import Transparent from '../Transparent'
 import EditCard from '../EditCard'
-import { useSocket } from '../../contexts/SocketProvider'
 import AttachmentsLink from '../AttachmentsLink'
 import useCardsServices from '../../services/useCardsServices'
+import pen from '../../images/pen.svg'
 
-export default function MyTasksTask({ teamId, project, list, card }) {
+const MyTasksTask = ({ teamId, project, list, card }) => {
     const inputRef = useRef(null)
     const [isInputActive, setIsInputActive] = useState(false)
-    const [showEditCard, setShowEditCard] = useState(false)
+    const [isEditCardActive, setIsEditCardActive] = useState(false)
     const [progress, setProgress] = useState('')
     const [isInputOk, setIsInputOk] = useState(true)
     const socket = useSocket()
@@ -23,6 +23,7 @@ export default function MyTasksTask({ teamId, project, list, card }) {
             setProgress('Add')
             return
         }
+
         setProgress(card.progress + '%')
     }, [setProgress, card.progress])
 
@@ -90,24 +91,27 @@ export default function MyTasksTask({ teamId, project, list, card }) {
                 />
             </div>
             <div className={styles.days}>
-                {card.progress === 100 ? (<div>Done</div>) :
-                    Date.parse(card.dueDate) === 0 || card.dueDate === null ? (<div>No Deadline</div>) :
-                        days < 0 ? (<div className={styles.deadline}>Deadline Passed</div>) :
-                            <div>{days}</div>
+                {card.progress === 100 
+                    ? <div>Done</div> 
+                    : Date.parse(card.dueDate) === 0 || card.dueDate === null || card.dueDate === undefined
+                        ? <div>No Deadline</div>
+                        : days < 0
+                            ? <div className={styles.deadline}>Deadline Passed</div>
+                            : <div>{days}</div>
                 }
             </div>
             <div className={styles['buttons-container']}>
                 {card.attachments.length > 0 && <AttachmentsLink card={card} project={project} teamId={teamId} />}
                 <ButtonClean
                     className={styles.button}
-                    onClick={() => setShowEditCard(true)}
+                    onClick={() => setIsEditCardActive(true)}
                     title={<img src={pen} alt='' width='14' />}
                 />
             </div>
-            { showEditCard && <div className={styles.edit}>
-                <Transparent hideForm={() => setShowEditCard(!showEditCard)} >
+            { isEditCardActive && <div className={styles.edit}>
+                <Transparent hideForm={() => setIsEditCardActive(!isEditCardActive)} >
                     <EditCard
-                        hideForm={() => setShowEditCard(!showEditCard)}
+                        hideForm={() => setIsEditCardActive(!isEditCardActive)}
                         initialCard={card}
                         listId={list._id}
                         project={project}
@@ -118,3 +122,5 @@ export default function MyTasksTask({ teamId, project, list, card }) {
         </div>
     )
 }
+
+export default MyTasksTask
